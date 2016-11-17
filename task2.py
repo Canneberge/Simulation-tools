e6b2511cf8d57b18180ee97bf77d70d3c41168f6
Loïc Masure <loic.masure@ensimag.grenoble-inp.fr>
2016-11-17 15:36:20 +0100

    Mise au propre du code
    En adéquation avec la PEP 8

--- task2.py
+++ task2.py
@@ -5,53 +5,54 @@ import matplotlib.pyplot as mpl
 import scipy.linalg as SL
 # from assimulo.solvers import CVode
 
+
 class BDF_3(Explicit_ODE):
     """
     BDF-3
     """
-    tol=1.e-8     
-    maxit=100     
-    maxsteps=500
-    
+    tol = 1.e-8
+    maxit = 100
+    maxsteps = 500
+
     def __init__(self, problem):
-        Explicit_ODE.__init__(self, problem) #Calls the base class
-        
-        #Solver options
+        Explicit_ODE.__init__(self, problem)  # Calls the base class
+
+        # Solver options
         self.options["h"] = 0.01
-        
-        #Statistics
+
+        # Statistics
         self.statistics["nsteps"] = 0
         self.statistics["nfcns"] = 0
-    
-    def _set_h(self,h):
+
+    def _set_h(self, h):
             self.options["h"] = float(h)
 
     def _get_h(self):
         return self.options["h"]
-        
-    h=property(_get_h,_set_h)
-        
+
+    h = property(_get_h, _set_h)
+
     def integrate(self, t, y, tf, opts):
         """
         _integrates (t,y) values until t > tf
         """
         h = self.options["h"]
         h = min(h, abs(tf-t))
-        
-        #Lists for storing the result
+
+        # Lists for storing the result
         tres = []
         yres = []
-        
+
         for i in range(self.maxsteps):
             if t >= tf:
                 break
             self.statistics["nsteps"] += 1
-            
-            if i==0:  # initial step
-                t_np1,y_np1 = self.step_EE(t,y, h)
+
+            if i == 0:  # initial step
+                t_np1, y_np1 = self.step_EE(t, y, h)
                 t, t_nm1 = t_np1, t
                 y, y_nm1 = y_np1, y
-            elif i==1:   
+            elif i == 1:
                 t_np1, y_np1 = self.step_BDF2([t, t_nm1], [y, y_nm1], h)
                 t, t_nm1, t_nm2 = t_np1, t, t_nm1
                 y, y_nm1, y_nm2 = y_np1, y, y_nm1
@@ -61,16 +62,17 @@ class BDF_3(Explicit_ODE):
                                               h)
                 t, t_nm1, t_nm2 = t_np1, t, t_nm1
                 y, y_nm1, y_nm2 = y_np1, y, y_nm1
-            
+
             tres.append(t)
             yres.append(y.copy())
-        
-            h=min(self.h,np.abs(tf-t))
+
+            h = min(self.h, np.abs(tf - t))
         else:
-            raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
-        
+            msg = "Final time not reached within maximum number of steps"
+            raise Explicit_ODE_Exception(msg)
+
         return ID_PY_OK, tres, yres
-    
+
     def step_EE(self, t, y, h):
         """
         This calculates the next step in the integration with explicit Euler.
@@ -104,7 +106,8 @@ class BDF_3(Explicit_ODE):
                 return t_np1, y_np1_ip1
             y_np1_i = y_np1_ip1
         else:
-            raise Explicit_ODE_Exception('Corrector could not converge within % iterations'%i)
+            msg = "Corrector could not converge within % iterations" % i
+            raise Explicit_ODE_Exception(msg)
 
     def step_BDF3(self, T, Y, h):
         """
@@ -130,38 +133,41 @@ class BDF_3(Explicit_ODE):
                 return t_np1, y_np1_ip1
             y_np1_i = y_np1_ip1
         else:
-            raise Explicit_ODE_Exception('Corrector could not converge within % iterations'%i)
-            
+            msg = "Corrector could not converge within % iterations" % i
+            raise Explicit_ODE_Exception(msg)
+
     def print_statistics(self, verbose=NORMAL):
         self.log_message('Final Run Statistics            : {name} \n'.format(name=self.problem.name),        verbose)
         self.log_message(' Step-length                    : {stepsize} '.format(stepsize=self.options["h"]), verbose)
         self.log_message(' Number of Steps                : '+str(self.statistics["nsteps"]),          verbose)               
         self.log_message(' Number of Function Evaluations : '+str(self.statistics["nfcns"]),         verbose)
-            
+
         self.log_message('\nSolver options:\n',                                    verbose)
         self.log_message(' Solver            : BDF3',                     verbose)
         self.log_message(' Solver type       : Fixed step\n',                      verbose)
-            
-#Define the rhs
-def f(t,y):
+
+
+# Define the rhs
+def f(t, y):
     ydot = -y[0]
     return np.array([ydot])
-    
-#Define an Assimulo problem
+
+# Define an Assimulo problem
 exp_mod = Explicit_Problem(f, 4)
 exp_mod.name = 'Simple BDF-3 Example'
 
-#Define another Assimulo problem
-def pend(t,y):
-    #g=9.81    l=0.7134354980239037
-    gl=13.7503671
-    return np.array([y[1],-gl*np.sin(y[0])])
-    
-pend_mod=Explicit_Problem(pend, y0=np.array([2.*np.pi,1.]))
-pend_mod.name='Nonlinear Pendulum'
 
-#Define an explicit solver
-exp_sim = BDF_3(pend_mod) #Create a BDF solver
+# Define another Assimulo problem
+def pend(t, y):
+    # g=9.81    l=0.7134354980239037
+    gl = 13.7503671
+    return np.array([y[1], -gl*np.sin(y[0])])
+
+pend_mod = Explicit_Problem(pend, y0=np.array([2. * np.pi, 1.]))
+pend_mod.name = 'Nonlinear Pendulum'
+
+# Define an explicit solver
+exp_sim = BDF_3(pend_mod)  # Create a BDF solver
 t, y = exp_sim.simulate(1)
 exp_sim.plot()
 mpl.show()
